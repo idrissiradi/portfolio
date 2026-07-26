3beb84e1a8ecca8e3c86f925b9aecb14ad7016af
Ahmed Idrissi Radi <idrissiradi@gmail.com>
2026-07-26 10:43:15 +0100

    Update CV link in portfolio data

--- src/constants/portfolio-data.ts
+++ src/constants/portfolio-data.ts
@@ -498,5 +498,5 @@ export const socialLinks = {
 	linkedin: 'https://www.linkedin.com/in/idrissi-radi-ahmed/',
 	x: 'https://x.com/idrissiradi',
 	email: 'idrissiradi@gmail.com',
-	cv: 'https://drive.google.com/file/d/1zg-chXL9FVpkIjhlgQe5gwL1NOOR7b0n/preview',
+	cv: 'https://drive.google.com/file/d/1QUDPuYCD0Y2Go0XERZYc9lYZOyLd-6GM/preview',
 };
